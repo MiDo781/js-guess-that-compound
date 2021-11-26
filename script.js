d78146d089e3dfc61b0c4b84b155cf6be33dc456
Ritz <culanagrv@outlook.com>
2021-11-26 09:11:26 +0800

    fix show and hide screen issue

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 // Learn Section
 const learnExitButton = document.querySelector(".button--exit--learn");
 
+// Main Menu
 const startButton = document.querySelector(".button--start");
-const exitButton = document.querySelector(".button--exit");
+const learnButton = document.querySelector(".button--learn");
+
+// Choices
+const choiceExitButton = document.querySelector(".button--exit");
 const choiceScreen = document.querySelector(".screen-choices");
-
-// Learn Section
-learnExitButton.addEventListener("click", () => {
-    window.open("/", "_self");
-});
-
-startButton.addEventListener("click", () => {
-    showScreen(choiceScreen)
-    console.log("working?")
-});
-
-exitButton.addEventListener("click", () => {
-    hideScreen(choiceScreen)
-    console.log("working?")
-});
 
 function showScreen(screen) {
     screen.style.opacity = "1";
