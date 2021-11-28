a3d7e65c68ed99218808b52fcc4318a62e93964b
Ritz <culanagrv@outlook.com>
2021-11-28 13:37:57 +0800

    add function to format subscripts

--- scripts/ionic.js
+++ scripts/ionic.js
@@ -1,7 +1,6 @@
 let ionic = [
     {
         compound: "H20",
-        subscript: "2",
         answer: "Water",
         choices: [
             "Water",
@@ -9,5 +8,44 @@ let ionic = [
             "Dioxide",
             "Chromium",
         ],
-    }
+        get subscripts() {
+            let subscripts = [
+                this.compound[1],
+            ]
+            return subscripts
+        }
+    },
+    {
+        compound: "Na2CO3",
+        answer: "Water",
+        choices: [
+            "Sodium Hipochlorite",
+            "Sodium Carbonate",
+            "Sodium Bicarbonate",
+            "Sodium Flouride",
+        ],
+        get subscripts() {
+            let subscripts = [
+                this.compound[2],
+                this.compound[5],
+            ]
+            return subscripts
+        }
+    },
+    {
+        compound: "H20",
+        answer: "Water",
+        choices: [
+            "Water",
+            "Chlorine",
+            "Dioxide",
+            "Chromium",
+        ],
+        get subscripts() {
+            let subscripts = [
+                this.compound[1],
+            ]
+            return subscripts
+        }
+    },
 ]
--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,13 @@
 const choiceScreen = document.querySelector(".screen--choices");
 const quizScreen = document.querySelector(".screen--quiz");
 const ionicScreen = document.querySelector(".screen--ionic");
-
-// Ionic
-
+// Pop-ups
+const popUp = document.querySelector(".screen--pop-up");
 // Buttons
 const ionicButton = document.querySelector(".button--ionic");
+
+let quizNumber = 1;
+
 ionicButton.addEventListener("click", () => {
     showScreen(ionicScreen);
 
@@ -15,15 +17,21 @@ ionicButton.addEventListener("click", () => {
     let compound = document.querySelector(".compound");
     let choices = document.getElementsByClassName("button--choice");
 
-    compound.innerHTML = ionic[0].compound;
+    compound.innerHTML = formatCompound(ionic, quizNumber);
+
     for (let i = 0; i < choices.length; i++) {
-        choices[i].innerHTML = ionic[0].choices[i];
+        choices[i].innerHTML = ionic[quizNumber].choices[i];
+        choices[i].addEventListener("click", () => {
+            console.log("I am " + choices[i].innerHTML);
+        });
     }
 
+    // Code for counter
     let counter = setInterval(() => {
         if (timer == 0) {
             timerText.innerHTML = timer--;  
             clearInterval(counter);
+            showPopUp(popUp);
         } else {
             if (timer <= 10) {
                 timerText.innerHTML = timer--;
@@ -35,6 +43,25 @@ ionicButton.addEventListener("click", () => {
     }, 1000);
 });
 
+function formatCompound(type, quizNumber) {
+    let finalCompound = "";
+    let initialCompound = type[quizNumber].compound.split("");
+    let subscripts = type[quizNumber].subscripts;
+    let subscriptIndex = 0;
+
+    for (let i = 0; i < initialCompound.length; i++) {
+        if (initialCompound[i].includes(subscripts[subscriptIndex])) {
+            initialCompound[i] = "<sub>" + subscripts[subscriptIndex] + "</sub>";
+            finalCompound += initialCompound[i];
+            subscriptIndex += 1;    
+        } else {
+            finalCompound += initialCompound[i];
+        }
+    }
+
+    return finalCompound;
+}
+
 function showScreen(screen) {
     screen.style.opacity = "1";
     screen.style.left = "50%";
@@ -43,4 +70,16 @@ function showScreen(screen) {
 function hideScreen(screen) {
     screen.style.opacity = "0";
     screen.style.left = "76%";
+}
+
+function showPopUp(popup) {
+    popup.style.opacity = "1";
+    popup.style.zIndex = "999";
+    popup.style.top = "50%";
+}
+
+function hidePopUp(popup) {
+    popup.style.opacity = "0";
+    popup.style.zIndex = "-1";
+    popup.style.top = "60%";
 }
