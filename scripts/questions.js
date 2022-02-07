9f17b10cb9ca68f5653663609499888af78c4c4e
MiDo <smidorima781@gmail.com>
2022-02-07 12:22:44 +0800

    Expected output of the project

---
+++ scripts/questions.js
@@ -0,0 +1,225 @@
+const ionic = [
+   {
+      compound: "KBr",
+      answer: "potassium bromide",
+      options: [
+         "potassium bromide",
+         "Sodium Carbonate",
+         "Sodium Calcium",
+         "potassium iodide",
+      ],
+      info: "This element is used in veterinary medicine for the treatment of epilepsy.",
+   },
+   {
+      compound: "Na₂CO₃",
+      answer: "Sodium Carbonate",
+      options: [
+         "Sodium Hipochlorite",
+         "Sodium Carbonate",
+         "Sodium Bicarbonate",
+         "Sodium Flouride",
+      ],
+      info: "Also known as washing soda, soda ash and soda crystals.",
+   },
+   {
+      compound: "NaCl",
+      answer: "Sodium Chloride",
+      options: [
+         "Sodium Chloride",
+         "Sodium and Chlorine",
+         "Sodium Chlorine",
+         "Sodium Calcium",
+      ],
+      info: "A chemical name for salt.",
+   },
+   {
+      compound: "NaHCO₃",
+      answer: "Sodium Bicarbonate",
+      options: [
+         "sodium fluoride",
+         "potassium iodide",
+         "sodium chloride",
+         "Sodium Bicarbonate",
+      ],
+      info: "Also known as baking soda or bicarbonate of soda.",
+   },
+   {
+      compound: "NaOCl",
+      answer: "Sodium Hypochlorite",
+      options: [
+         "Sodium Bicarbonate",
+         "potassium iodide",
+         "Sodium Bicarbonate",
+         "Sodium Hypochlorite",
+      ],
+      info: "It may also be viewed as the sodium salt of hypochlorous acid.",
+   },
+   {
+      compound: "CaCO₃",
+      answer: "calcium carbonate",
+      options: [
+         "magnesium hydroxide",
+         "aluminum hydroxide",
+         "sodium hydroxide",
+         "calcium carbonate",
+      ],
+      info: "It is a common substance found in rocks as the minerals calcite and aragonite and is the main component of eggshells, gastropod shells, shellfish skeletons and pearls.",
+   },
+   {
+      compound: "K₃PO₄",
+      answer: "potassium phosphate",
+      options: [
+         "potassium phosphate",
+         "calcium carbonate",
+         "sodium hydroxide",
+         "aluminum hydroxide",
+      ],
+      info: "Potassium phosphate is a generic term for the salts of potassium and phosphate ions.",
+   }, {
+      compound: "MgSO₄",
+      answer: "magnesium sulfate",
+      options: [
+         "potassium phosphate",
+         "calcium carbonate",
+         "magnesium sulfate",
+         "aluminum hydroxide",
+      ],
+      info: " It has a role as an anticonvulsant, a cardiovascular drug, a calcium channel blocker, etc.",
+   }, {
+      compound: "Na₂HPO₄",
+      answer: "sodium hydrogen phosphate",
+      options: [
+         "sodium hydrogen phosphate",
+         "potassium phosphate",
+         "calcium carbonate",
+         "aluminum hydroxide",
+      ],
+      info: "It is used in conjunction with trisodium phosphate in foods and water softening treatment.",
+   }, {
+      compound: "Na₂SO₃",
+      answer: "sodium sulfite",
+      options: [
+         "potassium phosphate",
+         "aluminum hydroxide",
+         "magnesium sulfate",
+         "sodium sulfite",
+      ],
+      info: "It is used commercially as an antioxidant and preservative.",
+   },
+]
+
+const covalent = [
+   {
+      compound: "O₂",
+      answer: "oxygen",
+      options: [
+         "oxygen",
+         "chlorine",
+         "hydrogen",
+         "carbon dioxide",
+      ],
+      info: "Oxygen, a colorless, odorless, tasteless gas essential to living organisms.",
+   },
+   {
+      compound: "Cl₂",
+      answer: "chlorine",
+      options: [
+         "oxygen",
+         "chlorine",
+         "hydrogen",
+         "carbon dioxide",
+      ],
+      info: "Used in industry and found in some household products.",
+   },
+   {
+      compound: "PCl₃",
+      answer: "phosphorus trichloride",
+      options: [
+         "phosphorus trichlorid",
+         "carbon dioxide",
+         "oxygen",
+         "hydrogen",
+      ],
+      info: "Being used for the manufacture of phosphites and other organophosphorus compounds.",
+   },
+   {
+      compound: "CH₃CH₂OH",
+      answer: "ethanol",
+      options: [
+         "ethanol",
+         "hydrogen",
+         "oxygen",
+         "carbon dioxide",
+      ],
+      info: "It is used as a solvent, in the synthesis of other organic chemicals, and as an additive to automotive gasoline.",
+   },
+   {
+      compound: "CH₄",
+      answer: "methane",
+      options: [
+         "methane",
+         "hydrogen",
+         "ethanol",
+         "carbon dioxide",
+      ],
+      info: "Major natural sources of methane include emissions from wetlands and oceans, and from the digestive processes of termites.",
+   },
+   {
+      compound: "NH₃",
+      answer: "ammonia",
+      options: [
+         "ammonia",
+         "methane",
+         "ethanol",
+         "hydrogen",
+      ],
+      info: "About 80% of the ammonia produced by industry is used in agriculture as fertilizer.",
+   },
+   {
+      compound: "H₂0",
+      answer: "Water",
+      options: [
+         "Water",
+         "Chlorine",
+         "Dioxide",
+         "Chromium",
+      ],
+      info: " It is made of two hydrogen atoms bonded to one oxygen atom. The hydrogen atoms each attach to one side of the oxygen atom and have a positive charge whereas the oxygen atom has a negative charge.",
+   },
+   {
+      compound: "HCl",
+      answer: "hydrogen chloride",
+      options: [
+         "hydrogen chloride",
+         "Water",
+         "Chlorine",
+         "hydrogen",
+      ],
+      info: "Hydrogen chloride can be formed during the burning of many plastics.",
+   },
+   {
+      compound: "O₃",
+      answer: "ozone",
+      options: [
+         "ozone",
+         "Water",
+         "hydrogen chloride",
+         "hydrogen",
+      ],
+      info: "A powerful oxidant (far more so than dioxygen) and has many industrial and consumer applications related to oxidation.",
+   },
+   {
+      compound: "CO₂",
+      answer: "carbon dioxide",  
+      options: [
+         "carbon dioxide",
+         "methane",
+         "ethanol",
+         "carbon",
+      ],
+      info: "can also be transformed by catalysts into polymers, the precursors for plastics, adhesives, and pharmaceuticals.",
+   },
+]
+
+export {ionic, covalent}
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
+import {ionic, covalent} from './questions.js'
+
 // Screens
 const selectScreen = document.querySelector(".screen--select");
 const quizScreen = document.querySelector(".screen--quiz");
+const backScreen = document.querySelector('.back--screenMenu');
+const backScreenCategory = document.querySelector('.back--screenCategory');
 const timeupScreen = document.querySelector(".screen--timeup");
 const wrongScreen = document.querySelector(".screen--wrong");
 const correctScreen = document.querySelector(".screen--correct");
@@ -8,245 +12,91 @@ const correctScreen = document.querySelector(".screen--correct");
 // Buttons
 const startButton = document.querySelector(".button--start");
 const learnButton = document.querySelector(".button--learn");
-const choiceButtons = document.getElementsByClassName("button--choice");
+const ionicButton = document.querySelector('.button--ionic');
+const covalentButton = document.querySelector('.button--covalent');
 const popupNextButtons = document.getElementsByClassName("popup__next");
 
 // For Quiz
 const optionButtons = document.getElementsByClassName("button--option");
 const compoundView = document.querySelector(".quiz__compound");
+const compoundInfo = document.querySelector('.compound__information');
 let number = 0;
 
 startButton.addEventListener("click", () => {
-   selectScreen.classList.add("screen--visible"); // add opacity 1 to show "Select Your Compounds" here, and output categories which is Ionic and Covalent
-
-   // Add event listeners to choice buttons
-   for (let i = 0; i < choiceButtons.length; i++) {
-      switch (choiceButtons[i].attributes["data-quiz"].value) {
-         case "ionic":
-            choiceButtons[i].setAttribute("onclick", "showQuestions(ionic)");
-            break;
-         case "covalent":
-            choiceButtons[i].setAttribute("onclick", "showQuestions(covalent)");
-            break;
-      }
-   }
+   selectScreen.classList.add("screen--visible");
+   // Listen for the user to select category
+   ionicButton.addEventListener('click', () => {
+      quizScreen.classList.add("screen--visible");
+      showQuestions(ionic); // get the array from import
+   });
+   covalentButton.addEventListener('click', () => {
+      quizScreen.classList.add("screen--visible");
+      showQuestions(covalent); // get the array from import
+   });
+   // Array Length == Total Questions
+   ionicButton.childNodes[3].textContent = `Total Items: ${ionic.length}`;
+   covalentButton.childNodes[3].textContent = `Total Items: ${covalent.length}`;
 });
 
-document.querySelector(".back--screenMenu").addEventListener("click", () => {
+backScreen.addEventListener("click", () => {
    selectScreen.classList.remove("screen--visible");
 });
 
-document.querySelector(".back--screenCategory").addEventListener("click", () => {
-   document.querySelector(".screen--quiz").style.zIndex = 0;
-   document.querySelector(".screen--quiz").style.opacity = 0;
-   document.querySelector(".screen--quiz").style.left = "60%";
+backScreenCategory.addEventListener("click", () => {
+   quizScreen.classList.remove("screen--visible");
 });
 
-function showQuestions(type) {
-   number = randomQuiz(type);
-   showScreen(quizScreen)
-   setCompound(type);
-   createOptions(type);
-}
+const showQuestions = (element) => {
+   //Generate Random Question and Options
+   let rand = Math.floor(Math.random() * element.length);
+   let randomQuestion = element[rand].compound;
+   let shuffleOptions = element[rand].options.sort(() => Math.random() - 0.5);
 
-function setCompound(type) {
-   let finalCompound = "";
-   let initialCompound = type[number].compound.split("");
-   let subscripts = type[number].subscripts;
-   let subscriptIndex = 0;
-   /*
-       -- Flow --
-       1. For each character in initial compound
-       2. Check if it includes a character from subscripts of index `subcriptIndex`
-       3. If true
-           3.1 Wrap thet current character's value with `<sub></sub>`
-           3.2 Add that current character to the variable `finalComound`
-           3.3 Increment subsciptIndex
-       4. If false
-           4.1 Add that current character to the variable `finalComound`
-   */
-   for (let i = 0; i < initialCompound.length; i++) {
-      if (initialCompound[i].includes(subscripts[subscriptIndex])) {
-         initialCompound[i] = "<sub>" + subscripts[subscriptIndex] + "</sub>";
-         finalCompound += initialCompound[i];
-         subscriptIndex += 1;
-      } else {
-         finalCompound += initialCompound[i];
+   // Insert <sub> tag per subscripts to be editable in css
+   let subscripts = /[\u2070-\u209F\u00B2\u00B3\u00B9]/g;
+   let modifiedRandomQuestion = [...randomQuestion];
+   for (let char = 0; char < randomQuestion.length; char++) {
+      if (randomQuestion[char].match(subscripts)) {
+         modifiedRandomQuestion[char] = `<sub>${randomQuestion[char].match(subscripts)}</sub>`;
       }
    }
-   compoundView.innerHTML = finalCompound;
-}
-
-function createOptions(type) {
-   for (let i = 0; i < type[number].options.length; i++) {
-      optionButtons[i].innerHTML = type[number].options[i];
+   
+   // Adding Question and Option to the document
+   compoundView.innerHTML = modifiedRandomQuestion.join('');
+   for (let option = 0; option < optionButtons.length; option++) {
+      optionButtons[option].textContent = shuffleOptions[option];
    }
-}
 
-function shuffleOptions(options) {
-   return options.sort(() => Math.random() - 0.5);
-}
+   // Listen response for user answer
+   Array.from(optionButtons).forEach((option) => {
+      option.addEventListener('click', () => {
+         let userAnswer = option.textContent;
+         let correctAnswer = element[rand].answer;
+         let answerInfo = element[rand].info;
 
-function checkAnswer(option, type) {
-   if (option.innerHTML == type[number].answer) {
-      console.log(true);
-      showNextQuiz(type);
-      // showScreen(correctScreen);
-   } else {
-      console.log(false);
-      showNextQuiz(type);
-      // showScreen(wrongScreen);
+         checkAnswer(userAnswer, correctAnswer, answerInfo)
+      });
+   });
+};
+
+function checkAnswer(user, key, info) {
+   if(user == key) {
+      correctScreen.classList.add('screen--visible');
+      compoundInfo.innerHTML = info;
+
+      wrongScreen.classList.remove('screen--visible');
    }
+
+   if (user != key) {
+      wrongScreen.classList.add('screen--visible');
+      correctScreen.classList.remove('screen--visible');
+   }
+
+   Array.from(popupNextButtons).forEach((btns) => {
+      btns.addEventListener('click', () => {
+         quizScreen.classList.remove("screen--visible");
+         correctScreen.classList.remove('screen--visible');
+         wrongScreen.classList.remove('screen--visible');
+      });
+   });
 }
-
-function randomQuiz(type) {
-   return Math.floor(Math.random() * type.length)
-}
-
-function showScreen(screen) { // use this to go back at the categories
-   screen.style.left = "50%";
-   screen.style.opacity = "1";
-   screen.style.zIndex = "1";
-}
-
-function hideScreen(screen) {
-   screen.style.left = "60%";
-   screen.style.opacity = "0";
-   screen.style.zIndex = "-1";
-}
-
-
-
-// // Screens
-// const choiceScreen = document.querySelector(".screen--choices");
-// const quizScreen = document.querySelector(".screen--quiz");
-// const ionicScreen = document.querySelector(".screen--ionic");
-// // Pop-ups
-// const popupTime = document.querySelector(".screen--pop-up");
-// const popupCorrect = document.querySelector(".screen--correct");
-// // Buttons
-// const choiceButton = document.getElementsByClassName("button--choices");
-// const quizButtonExit = document.querySelector(".button--exit--quiz");
-
-// // Quiz
-// let quizNumber = 0;
-// let compound = document.querySelector(".compound");
-// let choices = document.getElementsByClassName("button--choice")
-
-// // Timer
-// let counter;
-// let timer = 14;
-// let timerText = document.querySelector(".timer");
-
-// // Add onclick function to choice buttons
-// for (let i = 0; i < choiceButton.length; i++) {
-//     choiceButton[i].addEventListener("click", () => {
-//         if (choiceButton[i].getAttribute("data-quiz") == "ionic") {
-//             showQuiz(ionic)
-//         } else {
-//             showQuiz(covalent)
-//         }
-//     });
-// }
-
-// console.log(covalent[quizNumber].answer);
-
-// function showQuiz(type) {
-//     showScreen(quizScreen);
-
-//     // Set the compound text equal to the formatted compound
-//     compound.innerHTML = formatCompound(type, quizNumber);
-
-//     // Display Choices
-//     for (let i = 0; i < choices.length; i++) {
-//         choices[i].innerHTML = type[quizNumber].choices[i];
-//         choices[i].addEventListener("click", () => {
-//             checkAnswer(type, choices[i]);
-//         });
-//     }
-
-//     startTimer();
-// }
-
-// function formatCompound(name, quizNumber) {
-//     let finalCompound = "";
-//     let initialCompound = name[quizNumber].compound.split("");
-//     let subscripts = name[quizNumber].subscripts;
-//     let subscriptIndex = 0;
-
-//     for (let i = 0; i < initialCompound.length; i++) {
-//         if (initialCompound[i].includes(subscripts[subscriptIndex])) {
-//             initialCompound[i] = "<sub>" + subscripts[subscriptIndex] + "</sub>";
-//             finalCompound += initialCompound[i];
-//             subscriptIndex += 1;    
-//         } else {
-//             finalCompound += initialCompound[i];
-//         }
-//     }
-
-//     return finalCompound;
-// }
-
-// function checkAnswer(type, choice) {
-//     if (choice.innerHTML == type[quizNumber].answer) {
-//         console.log("correct");
-//         showQuiz();
-//     } else {
-//         console.log("try again");
-//         showQuiz();
-//     }
-// }
-
-// quizButtonExit.addEventListener("click", () => {
-//     stopTimer();
-//     hideScreen(quizScreen)
-// });
-
-// function startTimer() {
-//     timer = 14;
-//     counter = setInterval(() => {
-//         if (timer == 0) {
-//             timerText.innerHTML = timer--;  
-//             clearInterval(counter);
-//             showPopUp(popupTime);
-//         } else {
-//             if (timer <= 10) {
-//                 timerText.innerHTML = timer--;
-//                 timerText.style.color = "red";
-//             } else {
-//                 timerText.innerHTML = timer--;  
-//             }
-//         }
-//     }, 1000);
-// }
-
-// function stopTimer() {
-//     clearInterval(counter);
-//     timerText.innerHTML = 15; 
-//     timerText.style.color = "#111";
-//     timer = 14;
-// }
-
-// function showScreen(screen) {
-//     screen.style.opacity = "1";
-//     screen.style.left = "50%";
-//     screen.style.zIndex = "1";
-// }
-
-// function hideScreen(screen) {
-//     screen.style.opacity = "0";
-//     screen.style.left = "76%";
-//     screen.style.zIndex = "-2";
-// }
-
-// function showPopUp(popup) {
-//     popup.style.opacity = "1";
-//     popup.style.zIndex = "999";
-//     popup.style.top = "50%";
-// }
-
-// function hidePopUp(popup) {
-//     popup.style.opacity = "0";
-//     popup.style.zIndex = "-1";
-//     popup.style.top = "60%";
-// }
